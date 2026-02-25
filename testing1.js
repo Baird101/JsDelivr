ca250d87e5c9374cbf62ebe822aa3836dc8770f8
Baird101 <bairdilyon@gmail.com>
2026-02-25 10:59:47 -0800

    Add player object to testing1.js

--- testing1.js
+++ testing1.js
@@ -1,1 +1,2 @@
 var saveFiles = [{name:"ASbackup", score:9},{name:"Bristlefrost", score:16}, {name:"Cwalsh", score:12}, {name:"DebugRanger", score:16}, {name:"Eeek", score:6}];
+var player = {x:200,y:200};
