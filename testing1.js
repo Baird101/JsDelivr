9e944b68a27c24901e4b8297e70daaecd71f6bc7
Baird101 <bairdilyon@gmail.com>
2026-02-25 11:03:03 -0800

    Update saveFiles to only include 'Eeek'
    Removed all save files except for 'Eeek'.

--- testing1.js
+++ testing1.js
@@ -1,1 +1,1 @@
-var saveFiles = [{name:"ASbackup", score:9},{name:"Bristlefrost", score:16}, {name:"Cwalsh", score:12}, {name:"DebugRanger", score:16}, {name:"Eeek", score:6}];
+var saveFiles = [{name:"Eeek", score:6}];//{name:"ASbackup", score:9},{name:"Bristlefrost", score:16}, {name:"Cwalsh", score:12}, {name:"DebugRanger", score:16}, 
