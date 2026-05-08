572a49815c34f40fb9cc3d755019e2a8ef0d5015
Baird101 <bairdilyon@gmail.com>
2026-05-08 07:39:39 -0700

    Add additional save files to saveFiles array

--- testing1.js
+++ testing1.js
@@ -1,1 +1,1 @@
-var saveFiles = [{name:"Eeek", score:6}];//{name:"ASbackup", score:9},{name:"Bristlefrost", score:16}, {name:"Cwalsh", score:12}, {name:"DebugRanger", score:16}, 
+var saveFiles = [{name:"Eeek", score:6},{name:"ASbackup", score:9},{name:"Bristlefrost", score:16}, {name:"Cwalsh", score:12}, {name:"DebugRanger", score:16}];
